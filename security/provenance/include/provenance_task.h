8dc27c888102a16956508e17603743d77fcae409
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-10-13 07:30:37 -0700

    put lock around task prov

---
+++ security/provenance/include/provenance_task.h
@@ -0,0 +1,67 @@
+/*
+*
+* Author: Thomas Pasquier <tfjmp@seas.harvard.edu>
+*
+* Copyright (C) 2016 Harvard University
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2, as
+* published by the Free Software Foundation; either version 2 of the License, or
+*	(at your option) any later version.
+*
+*/
+#ifndef CONFIG_SECURITY_PROVENANCE_TASK
+#define CONFIG_SECURITY_PROVENANCE_TASK
+
+#include <linux/cred.h>
+
+#include "provenance_long.h" // for record_task_name
+#include "provenance_inode.h"
+
+static inline prov_msg_t* task_provenance( void ){
+	prov_msg_t* tprov = current_provenance();
+	lock_node(tprov);
+	if( provenance_is_recorded(tprov) ){ // the node has been recorded we need its name
+		record_task_name(current, tprov);
+	}
+	return tprov;
+}
+
+static inline void task_config_from_file(struct task_struct *task){
+	const struct cred *cred = get_task_cred(task);
+	struct mm_struct *mm;
+ 	struct file *exe_file;
+	struct inode *inode;
+	prov_msg_t* tprov;
+	prov_msg_t* iprov;
+
+	if(!cred)
+		return;
+
+	tprov = cred->provenance;
+
+	mm = get_task_mm(task);
+	if (!mm)
+ 		goto finished;
+	exe_file = get_mm_exe_file(mm);
+	mmput(mm);
+
+	if(exe_file){
+		inode = file_inode(exe_file);
+		iprov = inode_provenance(inode);
+		if(provenance_is_tracked(iprov)){
+			set_tracked(tprov);
+		}
+		if(provenance_is_opaque(iprov)){
+			set_opaque(tprov);
+		}
+		if(provenance_propagate(iprov)){
+			set_propagate(tprov);
+		}
+		put_prov(iprov);
+	}
+
+finished:
+	put_cred(cred);
+}
+#endif
