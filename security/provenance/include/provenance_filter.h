525a9c5a9988e33734ddbc32a695d2a7a311fc5e
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-01-14 13:03:29 -0800

    styling provenance_filter.h

--- security/provenance/include/provenance_filter.h
+++ security/provenance/include/provenance_filter.h
@@ -18,7 +18,7 @@
 extern bool prov_enabled;
 extern bool prov_all;
 
-#define HIT_FILTER(filter, data) ( (filter&data) != 0 )
+#define HIT_FILTER(filter, data) ((filter&data) != 0)
 
 extern uint64_t prov_node_filter;
 extern uint64_t prov_propagate_node_filter;
@@ -27,17 +27,18 @@ extern uint64_t prov_propagate_node_filter;
 #define filter_propagate_node(node) __filter_node(prov_propagate_node_filter, node)
 
 /* return either or not the node should be filtered out */
-static inline bool __filter_node(uint64_t filter, const prov_msg_t* node){
-  if(!prov_enabled){
+static inline bool __filter_node(uint64_t filter, const prov_msg_t* node)
+{
+  if (!prov_enabled) {
     return true;
   }
 
-  if(provenance_is_opaque(node)){
+  if (provenance_is_opaque(node)) {
     return true;
   }
 
   // we hit an element of the black list ignore
-  if( HIT_FILTER(filter, node_identifier(node).type) ){
+  if (HIT_FILTER(filter, node_identifier(node).type)) {
     return true;
   }
 
@@ -45,8 +46,9 @@ static inline bool __filter_node(uint64_t filter, const prov_msg_t* node){
 }
 
 #define UPDATE_FILTER (SUBTYPE(RL_VERSION_PROCESS)|SUBTYPE(RL_VERSION)|SUBTYPE(RL_NAMED))
-static inline bool filter_update_node(uint64_t relation_type, prov_msg_t* to){
-  if( HIT_FILTER(relation_type, UPDATE_FILTER) ){ // not update if relation is of above type
+static inline bool filter_update_node(uint64_t relation_type, prov_msg_t* to)
+{
+  if (HIT_FILTER(relation_type, UPDATE_FILTER)) { // not update if relation is of above type
     return true;
   }
   return false;
@@ -56,17 +58,18 @@ extern uint64_t prov_relation_filter;
 extern uint64_t prov_propagate_relation_filter;
 
 /* return either or not the relation should be filtered out */
-static inline bool filter_relation(uint64_t type, uint8_t allowed){
-  if(allowed==FLOW_DISALLOWED && HIT_FILTER(prov_relation_filter, RL_DISALLOWED)){
+static inline bool filter_relation(uint64_t type, uint8_t allowed)
+{
+  if (allowed == FLOW_DISALLOWED && HIT_FILTER(prov_relation_filter, RL_DISALLOWED)) {
     return true;
   }
 
-  if(allowed==FLOW_ALLOWED && HIT_FILTER(prov_relation_filter, RL_ALLOWED)){
+  if (allowed == FLOW_ALLOWED && HIT_FILTER(prov_relation_filter, RL_ALLOWED)) {
     return true;
   }
 
   // we hit an element of the black list ignore
-  if( HIT_FILTER(prov_relation_filter, type) ){
+  if (HIT_FILTER(prov_relation_filter, type)) {
     return true;
   }
 
@@ -74,30 +77,32 @@ static inline bool filter_relation(uint64_t type, uint8_t allowed){
 }
 
 /* return either or not tracking should propagate */
-static inline bool filter_propagate_relation(uint64_t type, uint8_t allowed){
-  if(allowed==FLOW_DISALLOWED && HIT_FILTER(prov_propagate_relation_filter, RL_DISALLOWED)){
+static inline bool filter_propagate_relation(uint64_t type, uint8_t allowed)
+{
+  if (allowed == FLOW_DISALLOWED && HIT_FILTER(prov_propagate_relation_filter, RL_DISALLOWED)) {
     return true;
   }
 
-  if(allowed==FLOW_ALLOWED && HIT_FILTER(prov_propagate_relation_filter, RL_ALLOWED)){
+  if (allowed == FLOW_ALLOWED && HIT_FILTER(prov_propagate_relation_filter, RL_ALLOWED)) {
     return true;
   }
 
   // the relation does not allow tracking propagation
-  if( HIT_FILTER(prov_propagate_relation_filter, type) ){
+  if (HIT_FILTER(prov_propagate_relation_filter, type)) {
     return true;
   }
 
   return false;
 }
 
-static inline bool should_record_relation(uint64_t type, prov_msg_t* from, prov_msg_t* to, uint8_t allowed){
+static inline bool should_record_relation(uint64_t type, prov_msg_t* from, prov_msg_t* to, uint8_t allowed)
+{
   // one of the node should not appear in the record, ignore the relation
-  if(filter_node(from) || filter_node(to)){
+  if (filter_node(from) || filter_node(to)) {
     return false;
   }
   // should the relation appear
-  if(filter_relation(type, allowed)){
+  if (filter_relation(type, allowed)) {
     return false;
   }
   return true;
