1e480a6367cc987ec0afbb36e957699321c0c191
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-01-14 13:08:49 -0800

    styling provenance_relay.h

--- security/provenance/include/provenance_relay.h
+++ security/provenance/include/provenance_relay.h
@@ -25,66 +25,69 @@
 #define PROV_INITIAL_BUFF_SIZE      1024*4
 #define PROV_INITIAL_LONG_BUFF_SIZE 256
 
-struct prov_boot_buffer{
+struct prov_boot_buffer {
   prov_msg_t  buffer[PROV_INITIAL_BUFF_SIZE];
   uint32_t    nb_entry;
 };
 
-struct prov_long_boot_buffer{
+struct prov_long_boot_buffer {
   long_prov_msg_t  buffer[PROV_INITIAL_LONG_BUFF_SIZE];
   uint32_t    nb_entry;
 };
 
-extern struct prov_boot_buffer* boot_buffer;
+extern struct prov_boot_buffer *boot_buffer;
 extern struct rchan *prov_chan;
 extern spinlock_t prov_chan_lock;
 
-static inline void prov_write(prov_msg_t* msg)
+static inline void prov_write(prov_msg_t *msg)
 {
   unsigned long flags;
 
   spin_lock_irqsave(&prov_chan_lock, flags);
   prov_jiffies(msg) = get_jiffies_64();
-  if( unlikely(prov_chan==NULL) ) // not set yet
+  if (unlikely(prov_chan == NULL)) // not set yet
   {
-    if( likely(boot_buffer->nb_entry < PROV_INITIAL_BUFF_SIZE) ){
+    if (likely(boot_buffer->nb_entry < PROV_INITIAL_BUFF_SIZE)) {
       memcpy(&(boot_buffer->buffer[boot_buffer->nb_entry]), msg, sizeof(prov_msg_t));
       boot_buffer->nb_entry++;
-    }else{
+    } else{
       printk(KERN_ERR "Provenance: boot buffer is full.\n");
     }
-  }else{
+  } else{
     relay_write(prov_chan, msg, sizeof(prov_msg_t));
   }
   spin_unlock_irqrestore(&prov_chan_lock, flags);
 }
 
 
-extern struct prov_long_boot_buffer* long_boot_buffer;
+extern struct prov_long_boot_buffer *long_boot_buffer;
 extern struct rchan *long_prov_chan;
 extern spinlock_t long_prov_chan_lock;
 
-static inline void long_prov_write(long_prov_msg_t* msg){
+static inline void long_prov_write(long_prov_msg_t* msg)
+{
   unsigned long flags;
 
   spin_lock_irqsave(&long_prov_chan_lock, flags);
   prov_jiffies(msg) = get_jiffies_64();
-  if( unlikely(long_prov_chan==NULL) ) // not set yet
+  if (unlikely(long_prov_chan == NULL)) // not set yet
   {
-    if( likely(long_boot_buffer->nb_entry < PROV_INITIAL_LONG_BUFF_SIZE) ){
+    if (likely(long_boot_buffer->nb_entry < PROV_INITIAL_LONG_BUFF_SIZE)) {
       memcpy(&long_boot_buffer->buffer[long_boot_buffer->nb_entry++], msg, sizeof(long_prov_msg_t));
-    }else{
+    } else{
       printk(KERN_ERR "Provenance: long boot buffer is full.\n");
     }
-  }else{
+  } else{
     relay_write(long_prov_chan, msg, sizeof(long_prov_msg_t));
   }
   spin_unlock_irqrestore(&long_prov_chan_lock, flags);
 }
 
 /* force sub-buffer switch */
-static inline void prov_flush( void ){
+static inline void prov_flush( void )
+{
   unsigned long flags;
+
   spin_lock_irqsave(&prov_chan_lock, flags);
   relay_flush(prov_chan);
   spin_unlock_irqrestore(&prov_chan_lock, flags);
