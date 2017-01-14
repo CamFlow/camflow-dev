92cc2203380807f36ed54876ab1887c225a0fb60
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-01-14 12:21:43 -0800

    started to fix some issues

--- security/provenance/include/av_utils.h
+++ security/provenance/include/av_utils.h
@@ -1,14 +1,14 @@
 /*
-*
-* Author: Thomas Pasquier <tfjmp@seas.harvard.edu>
-*
-* Copyright (C) 2016 Harvard University
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2, as
-* published by the Free Software Foundation.
-*
-*/
+ *
+ * Author: Thomas Pasquier <tfjmp@seas.harvard.edu>
+ *
+ * Copyright (C) 2016 Harvard University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation.
+ *
+ */
 
 #ifndef _AV_UTILS_PROVENANCE_H
 #define _AV_UTILS_PROVENANCE_H
--- security/provenance/include/provenance_secctx.h
+++ security/provenance/include/provenance_secctx.h
@@ -1,15 +1,15 @@
 /*
-*
-* Author: Thomas Pasquier <tfjmp@seas.harvard.edu>
-*
-* Copyright (C) 2016 Harvard University
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2, as
-* published by the Free Software Foundation; either version 2 of the License, or
-*	(at your option) any later version.
-*
-*/
+ *
+ * Author: Thomas Pasquier <tfjmp@seas.harvard.edu>
+ *
+ * Copyright (C) 2016 Harvard University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2, as
+ * published by the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ */
 #ifndef CONFIG_SECURITY_PROVENANCE_SECCTX
 #define CONFIG_SECURITY_PROVENANCE_SECCTX
 
