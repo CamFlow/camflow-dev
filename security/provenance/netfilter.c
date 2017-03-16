d9a57a58b9f53ec79e24fd29c7b2967b3412d48a
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-03-16 21:15:56 +0100

    fix license header

--- security/provenance/netfilter.c
+++ security/provenance/netfilter.c
@@ -6,11 +6,10 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2, as
- * published by the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
  *
  */
-
 #if defined(CONFIG_NETFILTER)
 
 #include "provenance.h"
