5b3043126e9f075f7a5070dfa173fa048d0b2c7d
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-10-17 20:40:14 +0000

    limit scope of variable in relay.c

--- security/provenance/relay.c
+++ security/provenance/relay.c
@@ -20,8 +20,8 @@
 #define LONG_PROV_BASE_NAME "long_provenance"
 
 /* global variable, extern in provenance.h */
-struct rchan *prov_chan;
-struct rchan *long_prov_chan;
+static struct rchan *prov_chan;
+static struct rchan *long_prov_chan;
 atomic64_t prov_relation_id = ATOMIC64_INIT(0);
 atomic64_t prov_node_id = ATOMIC64_INIT(0);
 
