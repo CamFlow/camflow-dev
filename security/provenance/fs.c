72cc49ccc3a74c6c78cc338cdda3be2b5f043e3c
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-10-28 23:20:27 +0000

    cleaner code

--- security/provenance/fs.c
+++ security/provenance/fs.c
@@ -524,13 +524,9 @@ static ssize_t prov_read_secctx(struct file *filp, char __user *buf,
 		return -ENOMEM;
 	data = (struct secinfo*)buf;
 
-	rtn = security_secid_to_secctx(data->secid, &ctx, &len);        // read secctx
-	if (rtn == -EOPNOTSUPP) {                                       // this is not supported by the main LSM
-		snprintf(data->secctx, PATH_MAX, "%d", data->secid);
-		rtn = 0;
-		goto out;
-	}else if (rtn < 0)
-		goto out;
+	rtn = security_secid_to_secctx(data->secid, &ctx, &len); // read secctx
+	if (rtn < 0)
+		return rtn;
 	if (len < PATH_MAX) {
 		if (copy_to_user(data->secctx, ctx, len)) {
 			rtn = -ENOMEM;
