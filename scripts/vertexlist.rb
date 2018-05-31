ea4fe7369bbcac50c2033c6c6f34e4a731004a8a
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-05-31 10:41:51 +0100

    should fix table formatting

--- scripts/vertexlist.rb
+++ scripts/vertexlist.rb
@@ -1,6 +1,7 @@
 puts "# Vertices supported by CamFlow\n\n"
 puts "Automatically generated do not edit!\n\n"
 puts '|As defined in #include<linux/provenance_types.h>|string in configuration file and CLI|'
+puts '---------------------------------------------------------------------------------------'
 File.readlines('./security/provenance/type.c').each do |line|
   relation = line.match(/\s*static\s*const\s*char\s*ND_STR_(\w+)\[\]\s*=\s*"(\w+)"\s*;/)
   if !relation.nil? && relation.captures[0] == 'TASK'
