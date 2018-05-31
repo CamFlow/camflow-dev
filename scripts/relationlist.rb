139a86082e3de92dd61eb9932dceb0f9a30c85fa
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-05-31 10:43:41 +0100

    hopefully good this time

--- scripts/relationlist.rb
+++ scripts/relationlist.rb
@@ -1,8 +1,8 @@
 puts "# Relations supported by CamFlow\n\n"
 puts "Automatically generated do not edit!\n\n"
-puts '|As defined in #include<linux/provenance_types.h>|string in configuration file and CLI|'
-puts '---------------------------------------------------------------------------------------'
+puts 'As defined in #include<linux/provenance_types.h>|string in configuration file and CLI|'
+puts '--------------------------------------------------------------------------------------'
 File.readlines('./security/provenance/type.c').each do |line|
   relation = line.match(/\s*static\s*const\s*char\s*RL_STR_(\w+)\[\]\s*=\s*"(\w+)"\s*;/)
-  puts '|RL_' + relation.captures[0] + '|' + relation.captures[1] + '|' unless relation.nil?
+  puts 'RL_' + relation.captures[0] + '|' + relation.captures[1] + '|' unless relation.nil?
 end
