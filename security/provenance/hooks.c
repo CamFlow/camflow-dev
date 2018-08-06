fd6c87a9346a013cc43d3c55a781d41a311ffab8
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-08-06 11:33:47 +0100

    change edge direction for mmap and unmmap (need to have a meeting with Michael and Margo to review the models)

--- security/provenance/hooks.c
+++ security/provenance/hooks.c
@@ -1302,15 +1302,15 @@ static int provenance_mmap_file(struct file *file,
 	if ((flags & MAP_TYPE) == MAP_SHARED
 	    || (flags & MAP_TYPE) == MAP_SHARED_VALIDATE) {
 		if ((prot & (PROT_WRITE)) != 0)
-			rc = uses(RL_MMAP_WRITE, iprov, tprov, cprov, file, flags);
+			rc = generates(RL_MMAP_WRITE, cprov, tprov, iprov, file, flags);
 		if (rc < 0)
 			goto out;
 		if ((prot & (PROT_READ)) != 0)
-			rc = uses(RL_MMAP_READ, iprov, tprov, cprov, file, flags);
+			rc = generates(RL_MMAP_READ, cprov, tprov, iprov, file, flags);
 		if (rc < 0)
 			goto out;
 		if ((prot & (PROT_EXEC)) != 0)
-			rc = uses(RL_MMAP_EXEC, iprov, tprov, cprov, file, flags);
+			rc = generates(RL_MMAP_EXEC, cprov, tprov, iprov, file, flags);
 	} else{
 		bprov = branch_mmap(iprov, cprov);
 		if (!bprov)
@@ -1319,15 +1319,15 @@ static int provenance_mmap_file(struct file *file,
 		if (rc < 0)
 			goto out;
 		if ((prot & (PROT_WRITE)) != 0)
-			rc = uses(RL_MMAP_WRITE, bprov, tprov, cprov, file, flags);
+			rc = generates(RL_MMAP_WRITE, cprov, tprov, bprov, file, flags);
 		if (rc < 0)
 			goto out;
 		if ((prot & (PROT_READ)) != 0)
-			rc = uses(RL_MMAP_READ, bprov, tprov, cprov, file, flags);
+			rc = generates(RL_MMAP_READ, cprov, tprov, bprov, file, flags);
 		if (rc < 0)
 			goto out;
 		if ((prot & (PROT_EXEC)) != 0)
-			rc = uses(RL_MMAP_EXEC, bprov, tprov, cprov, file, flags);
+			rc = generates(RL_MMAP_EXEC, cprov, tprov, bprov, file, flags);
 	}
 out:
 	spin_unlock(prov_lock(iprov));
@@ -1370,7 +1370,7 @@ static void provenance_mmap_munmap(struct mm_struct *mm,
 			iprov = file_provenance(mmapf, false);
 			spin_lock_irqsave_nested(prov_lock(cprov), irqflags, PROVENANCE_LOCK_PROC);
 			spin_lock_nested(prov_lock(iprov), PROVENANCE_LOCK_INODE);
-			uses(RL_MUNMAP, iprov, tprov, cprov, mmapf, flags);
+			generates(RL_MUNMAP, cprov, tprov, iprov, mmapf, flags);
 			spin_unlock(prov_lock(iprov));
 			spin_unlock_irqrestore(prov_lock(cprov), irqflags);
 		}
