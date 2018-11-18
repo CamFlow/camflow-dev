c2d81312ce85dad9598bc72d5e67a8847258019f
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-11-18 07:20:25 +0000

    update graph generation to follow latest change (i.e. processes get connected to machine)

--- scripts/instruction.rb
+++ scripts/instruction.rb
@@ -109,7 +109,7 @@ class Instruction
   end
 
   def self.get_cred_provenance_to_relation
-    return 'path-' + self.relation_to_str('RL_NAMED_PROCESS') + '->process_memory'
+    return 'path-' + self.relation_to_str('RL_NAMED_PROCESS') + '->process_memory,machine-' + self.relation_to_str('RL_RAN_ON') + '->process_memory' 
   end
 
   def self.inode_provenance_to_relation
