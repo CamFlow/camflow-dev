4a992443e00d8b0686bfc192d41705fbeb16a142
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2019-01-03 18:05:10 +0000

    ran uncrustify

--- security/provenance/hooks.c
+++ security/provenance/hooks.c
@@ -1815,7 +1815,7 @@ static int provenance_socket_post_create(struct socket *sock,
 		return -ENOMEM;
 
 	if (provenance_is_tracked(prov_elt(cprov))
-			|| provenance_is_tracked(prov_elt(tprov)))
+	    || provenance_is_tracked(prov_elt(tprov)))
 		set_tracked(prov_elt(iprov));
 
 	spin_lock_irqsave_nested(prov_lock(cprov), irqflags, PROVENANCE_LOCK_PROC);
