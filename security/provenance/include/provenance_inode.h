e346d1697d8e01201ca3189369d413a238bb575a
Michael Xueyuan Han <hanx@g.harvard.edu>
2019-03-29 11:12:18 -0400

    include explicit else branch in record_inode_name and record_inode_name_from_dentry functions

--- security/provenance/include/provenance_inode.h
+++ security/provenance/include/provenance_inode.h
@@ -138,16 +138,18 @@ static inline int record_inode_name_from_dentry(struct dentry *dentry,
 	if (provenance_is_name_recorded(prov_elt(prov)) ||
 	    !provenance_is_recorded(prov_elt(prov)))
 		return 0;
-	// Should not sleep.
-	buffer = kcalloc(PATH_MAX, sizeof(char), GFP_ATOMIC);
-	if (!buffer)
-		return -ENOMEM;
-	ptr = dentry_path_raw(dentry, buffer, PATH_MAX);
-	if (IS_ERR(ptr))
-		return PTR_ERR(ptr);
-	rc = record_node_name(prov, ptr, force);
-	kfree(buffer);
-	return rc;
+	else {
+        // Should not sleep.
+        buffer = kcalloc(PATH_MAX, sizeof(char), GFP_ATOMIC);
+        if (!buffer)
+            return -ENOMEM;
+        ptr = dentry_path_raw(dentry, buffer, PATH_MAX);
+        if (IS_ERR(ptr))
+            return PTR_ERR(ptr);
+        rc = record_node_name(prov, ptr, force);
+        kfree(buffer);
+        return rc;
+    }
 }
 
 /*!
@@ -169,12 +171,14 @@ static inline int record_inode_name(struct inode *inode, struct provenance *prov
 
 	if (provenance_is_name_recorded(prov_elt(prov)) || !provenance_is_recorded(prov_elt(prov)))
 		return 0;
-	dentry = d_find_alias(inode);
-	if (!dentry)    // We did not find a dentry, not sure if it should ever happen.
-		return 0;
-	rc = record_inode_name_from_dentry(dentry, prov, false);
-	dput(dentry);
-	return rc;
+	else {
+        dentry = d_find_alias(inode);
+        if (!dentry)    // We did not find a dentry, not sure if it should ever happen.
+            return 0;
+        rc = record_inode_name_from_dentry(dentry, prov, false);
+        dput(dentry);
+        return rc;
+    }
 }
 
 /*!
