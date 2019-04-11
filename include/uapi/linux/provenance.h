367eb5709253e857b1b3fe0f008bca62edec8ec4
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2019-04-11 09:40:36 +0100

    test all kernel build options

--- include/uapi/linux/provenance.h
+++ include/uapi/linux/provenance.h
@@ -29,7 +29,7 @@
 	"."xstr (CAMFLOW_VERSION_MINOR)					\
 	"."xstr (CAMFLOW_VERSION_PATCH)					\
 
-#define CAMFLOW_COMMIT "59b9d73c888fa4160d18b4c2b5cd1ed954704660"
+#define CAMFLOW_COMMIT "e749dc5be368a88c7886982aadecf8b485d7c086"
 
 #define PROVENANCE_HASH                 "sha256"
 
