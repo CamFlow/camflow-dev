cd705d0b12072f7f8b0573461b56368271cbe3ac
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2021-11-01 16:15:11 -0700

    update clang to version 13 (hopefully the fix build issue)

--- include/uapi/linux/provenance.h
+++ include/uapi/linux/provenance.h
@@ -40,7 +40,7 @@
 	"."xstr (CAMFLOW_VERSION_MINOR)					\
 	"."xstr (CAMFLOW_VERSION_PATCH)					\
 
-#define CAMFLOW_COMMIT ""
+#define CAMFLOW_COMMIT "f989f56d8e1929c6708d9196716bd06e95a322ee"
 
 #define PROVENANCE_HASH                 "sha256"
 
