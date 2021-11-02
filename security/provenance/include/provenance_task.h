d883c9f6ddb8e80dea3691f03d4388011b258f7d
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2021-11-02 14:11:16 -0700

    remove dead code

--- security/provenance/include/provenance_task.h
+++ security/provenance/include/provenance_task.h
@@ -412,101 +412,24 @@ static inline struct provenance *prov_from_vpid(pid_t pid)
  * See fs/exec.c
  *
  */
-static inline void acct_arg_size(struct linux_binprm *bprm, unsigned long pages)
-{
-	struct mm_struct *mm = current->mm;
-	long diff = (long)(pages - bprm->vma_pages);
-
-	if (!mm || !diff)
-		return;
-
-	bprm->vma_pages = pages;
-	add_mm_counter(mm, MM_ANONPAGES, diff);
-}
-
-/*!
- * Helper function used to copy arguments.
- * See fs/exec.c
- *
- */
-static inline struct page *get_arg_page(struct linux_binprm *bprm,
-					unsigned long pos,
-					int write)
+static inline struct page *__get_arg_page_r(struct linux_binprm *bprm,
+					    unsigned long pos)
 {
 	struct page *page;
 	int ret;
-	unsigned int gup_flags = FOLL_FORCE;
-
-#ifdef CONFIG_STACK_GROWSUP
-	if (write) {
-		ret = expand_downwards(bprm->vma, pos);
-		if (ret < 0)
-			return NULL;
-	}
-#endif
-
-	if (write)
-		gup_flags |= FOLL_WRITE;
 
 	/*
 	 * We are doing an exec().  'current' is the process
 	 * doing the exec and bprm->mm is the new process's mm.
 	 */
 	mmap_read_lock(bprm->mm);
-	ret = get_user_pages_remote(bprm->mm, pos, 1, gup_flags,
+	ret = get_user_pages_remote(bprm->mm, pos, 1, FOLL_FORCE,
 				    &page, NULL, NULL);
 	mmap_read_unlock(bprm->mm);
 	if (ret <= 0)
 		return NULL;
 
-	if (write) {
-		unsigned long size = bprm->vma->vm_end - bprm->vma->vm_start;
-		unsigned long ptr_size;
-		struct rlimit *rlim;
-
-		/*
-		 * Since the stack will hold pointers to the strings, we
-		 * must account for them as well.
-		 *
-		 * The size calculation is the entire vma while each arg page is
-		 * built, so each time we get here it's calculating how far it
-		 * is currently (rather than each call being just the newly
-		 * added size from the arg page).  As a result, we need to
-		 * always add the entire size of the pointers, so that on the
-		 * last call to get_arg_page() we'll actually have the entire
-		 * correct size.
-		 */
-		ptr_size = (bprm->argc + bprm->envc) * sizeof(void *);
-		if (ptr_size > ULONG_MAX - size)
-			goto fail;
-		size += ptr_size;
-
-		acct_arg_size(bprm, size / PAGE_SIZE);
-
-		/*
-		 * We've historically supported up to 32 pages (ARG_MAX)
-		 * of argument strings even with small stacks
-		 */
-		if (size <= ARG_MAX)
-			return page;
-
-		/*
-		 * Limit to 1/4-th the stack size for the argv+env strings.
-		 * This ensures that:
-		 *  - the remaining binfmt code will not run out of stack space,
-		 *  - the program will have a reasonable amount of stack left
-		 *    to work from.
-		 */
-		rlim = current->signal->rlim;
-		if (size > READ_ONCE(rlim[RLIMIT_STACK].rlim_cur) / 4)
-			goto fail;
-	}
-
 	return page;
-
-fail:
-	put_page(page);
-	return NULL;
 }
 
 /*!
@@ -526,7 +449,7 @@ static inline int copy_argv_bprm(struct linux_binprm *bprm, char *buff,
 	pos = bprm->p;
 	ofs = pos % PAGE_SIZE;
 	while (len) {
-		new_page = get_arg_page(bprm, pos, 0);
+		new_page = __get_arg_page_r(bprm, pos);
 		if (!new_page) {
 			rv = -E2BIG;
 			goto out;
